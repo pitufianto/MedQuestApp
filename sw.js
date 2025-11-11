0435950346f0034648920e1252abf1a1b0e59575
pitufianto <antoniasofialagoslorca@gmail.com>
2025-11-11 19:58:45 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,17 @@
-const CACHE_NAME = 'medquest-v1';
 // Lista de archivos para guardar en caché
+const CACHE_NAME = 'medquest-v2'; // <-- ¡Cambiado a v2!
 const urlsToCache = [
     '/',
     'index.html',
     'style.css',
     'script.js',
+    // ¡LINKS DE ÍCONOS ARREGLADOS!
+    'https://cdn-icons-png.flaticon.com/192/869/869636.png',
+    'https://cdn-icons-png.flaticon.com/512/869/869636.png',
+    
     'https://cdn.jsdelivr.net/npm/chart.js',
     'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap',
-    'https://fonts.gstatic.com/s/nunito/v26/XRXV3I6Li01BKofINeaB.woff2' // Fuente Nunito
+    'https://fonts.gstatic.com/s/nunito/v26/XRXV3I6Li01BKofINeaB.woff2'
 ];
 
 // Evento de Instalación: Se guarda todo en caché
